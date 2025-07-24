4a089e544c8fa8bf9ea8f4d6f0be2104351e1e5d
wkj2333666 <2695071759@qq.com>
2025-07-24 10:36:29 +0800

    refactor: move camera init to camera_init.h

--- router.h
+++ router.h
@@ -21,3 +21,16 @@ void handleRoot() {
     "</body></html>");
 }
 
+void handleGetImage() {
+  server.send(200, "text/html", 
+    "<html><body>"
+    "<h1>SmartFridge API</h1>"
+    // "<p>可用端点：</p>"
+    "<ul>"
+    // "<li>GET /api/sensors - 获取传感器数据</li>"
+    // "<li>POST /api/control - 控制设备</li>"
+    // "<li>GET /api/status - 系统状态</li>"
+    "<li>Hello World!</li>"
+    "</ul>"
+    "</body></html>");
+}
